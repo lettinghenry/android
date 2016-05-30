68d081da995fcd49412787a802f471c4469783c9
lettinghenry <lettinghenry0@gmail.com>
2016-05-30 15:53:38 +0300

    initial android versions

---
+++ Helloonwindows/app/src/main/java/com/example/henry/helloonwindows/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.henry.helloonwindows;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button diamond;
+    Button gold;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        diamond=(Button) findViewById(R.id.butndi)
+    }
+}
---
+++ Helloonwindows/app/src/main/java/com/example/henry/helloonwindows/girls.java
@@ -0,0 +1,14 @@
+package com.example.henry.helloonwindows;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class girls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_girls);
+    }
+
+}
